e04fa28a43bf9fe198d5fe2f7265be1cfa815a91
erxck <erickrian2002@gmail.com>
2023-08-16 00:25:51 -0300

    bug fixed

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -16,7 +16,7 @@ export default function Blog() {
       title: "Pesquisa Binária e Pesquisa Linear",
       description:
         "Explorando as diferenças entre pesquisa binária e pesquisa linear: uma análise comparativa dos algoritmos de busca mais populares.",
-      date: "10 de Agosto de 2023",
+      date: "02 de Abril de 2023",
     },
     {
       title: "O que é um algoritmo?",
